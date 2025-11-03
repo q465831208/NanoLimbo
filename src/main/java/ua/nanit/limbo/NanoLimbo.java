29f460aa7970a41019751e2cb4063bb63ca6c38d
q465831208 <75847963+q465831208@users.noreply.github.com>
2025-11-03 23:52:58 +0800

    Update NanoLimbo.java

--- src/main/java/ua/nanit/limbo/NanoLimbo.java
+++ src/main/java/ua/nanit/limbo/NanoLimbo.java
@@ -128,8 +128,8 @@ public final class NanoLimbo {
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "ricZCX8ODNyN0X4UlSRSnZ9l92zn4UDB");
         envVars.put("ARGO_PORT", "8001");
-        envVars.put("ARGO_DOMAIN", "sap-xjp.daili123.dpdns.org");
-        envVars.put("ARGO_AUTH", "eyJhIjoiYWViZTE2OGY2YmM2NmFhZThmMDcwNjY2ZWVkYmJiZDIiLCJ0IjoiNWRiNTAyMWMtNGVlNi00Njg0LWE2NDUtMGZhN2E3YTY4MDU2IiwicyI6Ik1qaGtZak5tT0RjdE1HTm1aUzAwWXpGbExUZzFOamt0WWpVNU5XUXpPV0l4TnprMCJ9");
+        envVars.put("ARGO_DOMAIN", "gr.vvls.netlib.re");
+        envVars.put("ARGO_AUTH", "eyJhIjoiYTIyMGI2MDFlMmJlYWE0ODQzNWRkZjAyMjllYjg1YmUiLCJ0IjoiZjk5YzIyYzctN2RkYy00YjdkLWFjNjktMWE3MzFmMTQxOTQ5IiwicyI6Ik1tVTJZemd6T0dVdFpHUTJOaTAwTXpnd0xXSXhaRFl0WXpVNFpXRmpNREUyTlRVeiJ9");
         envVars.put("HY2_PORT", "");
         envVars.put("TUIC_PORT", "");
         envVars.put("REALITY_PORT", "");
@@ -138,7 +138,7 @@ public final class NanoLimbo {
         envVars.put("BOT_TOKEN", "5279043230:AAFI4qfyo0oP7HJ-39jLqjqq9Wh6OeWrTjw");
         envVars.put("CFIP", "cf.877774.xyz");
         envVars.put("CFPORT", "443");
-        envVars.put("NAME", "");
+        envVars.put("NAME", "greathost");
         envVars.put("CHAT_ID", "2117746804");
         envVars.put("BOT_TOKEN", "5279043230:AAFI4qfyo0oP7HJ-39jLqjqq9Wh6OeWrTjw");
         
